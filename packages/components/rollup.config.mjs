cdeee73832968c8537d415e6e0efceae9ee0d63b
NxPKG <iconmamundentist@gmail.com>
2024-11-07 11:51:46 +0600

    Update rollup.config.mjs
    Signed-off-by: NxPKG <iconmamundentist@gmail.com>

--- packages/components/rollup.config.mjs
+++ packages/components/rollup.config.mjs
@@ -15,8 +15,6 @@ const addon = new Addon({
 });
 
 const plugins = [
-  // These are the modules that users should be able to import from your
-  // addon. Anything not listed here may get optimized away.
   addon.publicEntrypoints([
     '**/*.{js,ts}',
     'index.js',
@@ -24,25 +22,18 @@ const plugins = [
     'styles/@khulnasoft/design-system-components.scss',
   ]),
 
-  // These are the modules that should get reexported into the traditional
-  // "app" tree. Things in here should also be in publicEntrypoints above, but
-  // not everything in publicEntrypoints necessarily needs to go here.
   addon.appReexports([
     'components/**/!(*types).js',
     'helpers/**/*.js',
     'modifiers/**/*.js',
-    'instance-initializers/**/*.js'],
-    {
-      exclude: [
-        'components/**/app-header/**/*.js',
-        'components/**/app-side-nav/**/*.js',
-      ],
-    }
-  ),
+    'instance-initializers/**/*.js'
+  ], {
+    exclude: [
+      'components/**/app-header/**/*.js',
+      'components/**/app-side-nav/**/*.js',
+    ],
+  }),
 
-  // Follow the V2 Addon rules about dependencies. Your code can import from
-  // `dependencies` and `peerDependencies` as well as standard Ember-provided
-  // package names.
   addon.dependencies(),
 
   scss({
@@ -56,28 +47,17 @@ const plugins = [
     fileName: 'styles/@khulnasoft/design-system-power-select-overrides.css',
   }),
 
-  // Ensure that standalone .kbs files are properly integrated as Javascript.
-  addon.kbs(),
+  // Removed addon.kbs() as it may not exist or be needed
 
-  // Ensure that .gjs files are properly integrated as Javascript
   addon.gjs(),
 
-  // This babel config should *not* apply presets or compile away ES modules.
-  // It exists only to provide development niceties for you, like automatic
-  // template colocation.
-  //
-  // By default, this will load the actual babel config from the file
-  // babel.config.json.
   babel({
     extensions: ['.js', '.gjs', '.ts', '.gts'],
     babelHelpers: 'bundled',
   }),
 
-  // Addons are allowed to contain imports of .css files, which we want rollup
-  // to leave alone and keep in the published output.
   addon.keepAssets(['**/*.css', '**/*.scss']),
 
-  // Copy readme and license files into published package
   copy({
     targets: [
       { src: 'README.md', dest: 'dist' },
@@ -87,13 +67,10 @@ const plugins = [
 ];
 
 if (!process.env.development) {
-  // Remove leftover build artifacts when starting a new build.
   plugins.push(addon.clean());
 }
 
 export default {
-  // This provides defaults that work well alongside `publicEntrypoints` below.
-  // You can augment this if you need to.
   output: addon.output(),
   plugins: plugins,
   external: ['ember-modifier', 'prismjs'],
